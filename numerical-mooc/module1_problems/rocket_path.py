1d47243aa0d406e9639015c96761478c6bf4e1da
Rohen <mailme.rohen@gmail.com>
2016-10-01 17:13:40 +0400

    Finished decision trees

--- numerical-mooc/module1_problems/rocket_path.py
+++ numerical-mooc/module1_problems/rocket_path.py
@@ -10,15 +10,38 @@ a = np.pi * r**2 # max cross sectional area of rocket
 ve = 325 # m/s. exhaust speed
 cd = 0.15 # drag coefficient
 mp = 100 # 100kg at time t. initial weight of the propellant.
+mp_dot = 20.
 
 # Weight of rocket a particular time.
 def mp1(t0, t):
   if(t <= 5.):
-    return 20. * t - 20 * t0
+    return mp_dot * t - mp_dot * t0
   elif(t > 5.):
     return 0.
 
 m = mp - mp1(0., 3.2)
 print m
 
-#Maximum Velocity
+#Velocity and Height
+h0 = 0
+v0 = 0
+
+def f(u):
+  # u is array of float containing values at time t
+  h, v = u
+  return np.array([v - g + (mp_dot * ve) / (ms + mp) - (0.5 * p * v * np.abs(v) * a * cd) / (ms + mp)])
+
+def euler(u, f, dt):
+  return u + (f(u) * dt)
+
+T = 50 # final time
+dt = 0.1 # given
+t = np.arange(0, T + dt, dt)
+N = len(t)
+
+u = np.empty((N, 2)) #creates a matrix
+u[0] = np.array([h0, v0])
+
+#Using the method
+
+
